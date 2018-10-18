20d7da20f476213b6138f2c53d1cd3d27a640c2c
paul.hwang <paul.hwang@getac.com.tw>
2018-10-18 17:06:48 +0800

    view

--- PhwangGo/Controllers/HomeController.cs
+++ PhwangGo/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace PhwangGo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public ActionResult HomeRoot()
+        public IActionResult HomeRoot()
         {
             ViewBag.HomeRoot = "HomeRoot";
             return View();
