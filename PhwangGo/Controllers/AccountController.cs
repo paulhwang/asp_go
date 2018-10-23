9f99ba3cd23f035ad75b1a457a1f601668e88fc9
paul.hwang <paul.hwang@getac.com.tw>
2018-10-23 14:36:58 +0800

    ajax

--- PhwangGo/Controllers/AccountController.cs
+++ PhwangGo/Controllers/AccountController.cs
@@ -26,26 +26,6 @@ namespace PhwangGo.Controllers
             Debug.WriteLine("in AccountSignIn()");
             return View();
         }
-      
-        [HttpGet]
-        public string AccountSignInReq()
-        {
-            Fabric.FabricRootClass fabric_root = Fabric.GlobalVariableClass.getGoRoot();
-            if (fabric_root == null)
-            {
-                return "junk";
-            }
-            
-            Debug.WriteLine("in AccountSignInReq()");
-            if (Request == null)
-            {
-                Debug.WriteLine("null data");
-                return "null request";
-            }
-
-            string input_data = Request.Headers["phwangajaxrequest"];
-            return fabric_root.ajaxFabricServiceObject.parseAjaxPacket(input_data);
-        }
 
         public IActionResult AccountSignUp()
         {
---
+++ PhwangGo/Controllers/AjaxController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhwangGo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        [HttpGet]
+        public string AjaxGetRequest(string var)
+        {
+            return AjaxCommonFunction(var);
+        }
+
+        [HttpPost]
+        public string AjaxPostRequest(string var)
+        {
+            return AjaxCommonFunction(var);
+        }
+
+        [HttpPut]
+        public string AjaxPutRequest(string var)
+        {
+            return AjaxCommonFunction(var);
+        }
+
+        private string AjaxCommonFunction(string var)
+        { 
+            Fabric.FabricRootClass fabric_root = Fabric.GlobalVariableClass.getGoRoot();
+            if (fabric_root == null)
+            {
+                return "junk";
+            }
+
+            Debug.WriteLine("in AccountSignInReq()");
+            if (Request == null)
+            {
+                Debug.WriteLine("null data");
+                return "null request";
+            }
+
+            string input_data = Request.Headers["phwangajaxrequest"];
+            return fabric_root.ajaxFabricServiceObject.parseAjaxPacket(input_data);
+        }
+    }
+}
