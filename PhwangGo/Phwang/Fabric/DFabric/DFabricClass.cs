19dcb2498fbc830b1df8b28af6050f482f1ff0a5
paul.hwang <paul.hwang@getac.com.tw>
2018-10-26 23:08:11 +0800

    .

--- PhwangGo/Phwang/Fabric/DFabric/DFabricClass.cs
+++ PhwangGo/Phwang/Fabric/DFabric/DFabricClass.cs
@@ -6,14 +6,10 @@
  ******************************************************************************
  */
 
- using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
