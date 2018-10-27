39bbce5da37d8ddddb3c8cac999f25eaafb708a9
paul.hwang <paul.hwang@getac.com.tw>
2018-10-27 21:57:54 +0800

    map

---
+++ PhwangGo/Phwang/FrontEnd/FrantEndAjaxMapClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.FrontEnd
+{
+    public class FrantEndAjaxMapClass
+    {
+        public string ajaxIdStr { get; }
+
+        public FrantEndAjaxMapClass(string ajax_id_str_val)
+        {
+            this.ajaxIdStr = ajax_id_str_val;
+        }
+    }
+}
