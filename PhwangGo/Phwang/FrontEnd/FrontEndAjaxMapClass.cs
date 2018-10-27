d5f1fccac6c795b7cbb05487e53f9f43bf2fe8f4
paul.hwang <paul.hwang@getac.com.tw>
2018-10-27 22:46:01 +0800

    pending

--- PhwangGo/Phwang/FrontEnd/FrontEndAjaxMapClass.cs
+++ PhwangGo/Phwang/FrontEnd/FrontEndAjaxMapClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Phwang.FrontEnd
@@ -8,10 +9,22 @@ namespace Phwang.FrontEnd
     public class FrontEndAjaxMapClass
     {
         public string ajaxIdStr { get; }
+        private ManualResetEvent theSignal { get; }
 
         public FrontEndAjaxMapClass(string ajax_id_str_val)
         {
             this.ajaxIdStr = ajax_id_str_val;
+            this.theSignal = new ManualResetEvent(false);
+        }
+
+        public void PendingTillWorkDone()
+        {
+            this.theSignal.WaitOne();
+        }
+
+        public void WakeUpPendingThread()
+        {
+            this.theSignal.Set();
         }
     }
 }
