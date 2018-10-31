0250e2fb41e4b5fb3c05ea3799bb5b4f35bda506
paul.hwang <paul.hwang@getac.com.tw>
2018-10-31 16:19:10 +0800

    .

---
+++ PhwangGo/Phwang/Protocols/FabricFrontEndProtocolClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Protocols
+{
+    public class FabricFrontEndProtocolClass
+    {
+        public const int LINK_MGR_PROTOCOL_TRANSPORT_PORT_NUMBER = 8006;
+
+        public const int AJAX_MAPING_ID_SIZE = 3;
+        public const int WEB_FABRIC_PROTOCOL_NAME_LIST_TAG_SIZE = 3;
+
+        //#define WEB_FABRIC_PROTOCOL_RESPOND_IS_GET_LINK_DATA_PENDING_SESSION 'S'
+        //#define WEB_FABRIC_PROTOCOL_RESPOND_IS_GET_LINK_DATA_PENDING_SESSION3 'T'
+        //#define WEB_FABRIC_PROTOCOL_RESPOND_IS_GET_LINK_DATA_PENDING_DATA 'D'
+        public const String WEB_FABRIC_PROTOCOL_RESPOND_IS_GET_LINK_DATA_NAME_LIST = "N";
+
+        //#define LINK_MGR_DATA_BUFFER_SIZE 512
+
+        public const int LINK_MGR_PROTOCOL_LINK_ID_SIZE = 4;
+        //public const int LINK_MGR_PROTOCOL_LINK_INDEX_SIZE = 4;
+        //public const int LINK_MGR_PROTOCOL_LINK_ID_INDEX_SIZE = (LINK_MGR_PROTOCOL_LINK_ID_SIZE + LINK_MGR_PROTOCOL_LINK_INDEX_SIZE);
+
+        public const int LINK_MGR_PROTOCOL_SESSION_ID_SIZE = 4;
+            //#define LINK_MGR_PROTOCOL_SESSION_INDEX_SIZE 4
+            //#define LINK_MGR_PROTOCOL_SESSION_ID_INDEX_SIZE (LINK_MGR_PROTOCOL_SESSION_ID_SIZE + LINK_MGR_PROTOCOL_SESSION_INDEX_SIZE)
+    }
+}
---
+++ PhwangGo/Phwang/Protocols/FabricThemeProtocolClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Protocols
+{
+    public class FabricThemeProtocolClass
+    {
+        public const string FABRIC_THEME_PROTOCOL_COMMAND_IS_SETUP_ROOM = "R";
+        //#define FABRIC_THEME_PROTOCOL_RESPOND_IS_SETUP_ROOM 'r'
+        //#define FABRIC_THEME_PROTOCOL_COMMAND_IS_PUT_ROOM_DATA 'D'
+        //#define FABRIC_THEME_PROTOCOL_RESPOND_IS_PUT_ROOM_DATA 'd'
+    }
+}
