a4770f79a8d9337a9d09cdeb423c4525a7f30f72
paul.hwang <paul.hwang@getac.com.tw>
2018-10-31 23:24:30 +0800

    global

--- PhwangGo/Phwang/GlobalVariables/GlobacVariables.cs
+++ PhwangGo/Phwang/GlobalVariables/GlobacVariables.cs
@@ -7,22 +7,26 @@ namespace Phwang.GlobalVariables
 {
     public class GlobalVariableClass
     {
-        static public FrontEnd.FrontEndRootClass FrontEndRootObject { get; set; }
-        static public Fabric.FabricRootClass FabricRootObject { get; set; }
+        static public FrontEnd.FrontEndRootClass frontEndRootObject { get; set; }
+        static public Fabric.FabricRootClass fabricRootObject { get; set; }
+        static public Theme.ThemeRootClass themeRootObject { get; set; }
+        static public Engine.EngineRootClass engineRootObject { get; set; }
 
         public static void Initilization()
         {
-            if (FrontEndRootObject == null)
+            if (frontEndRootObject == null)
             {
-                FabricRootObject = new Fabric.FabricRootClass();
-                FrontEndRootObject = new FrontEnd.FrontEndRootClass();
+                fabricRootObject = new Fabric.FabricRootClass();
+                frontEndRootObject = new FrontEnd.FrontEndRootClass();
+                themeRootObject = new Theme.ThemeRootClass();
+                engineRootObject = new Engine.EngineRootClass();
             }
         }
 
         public static FrontEnd.FrontEndRootClass getGoRoot()
         {
             Initilization();
-            return FrontEndRootObject;
+            return frontEndRootObject;
         }
     }
 }
