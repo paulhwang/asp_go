62350077d41f1cb12dfa74040acab260aafb4a79
paul.hwang <paul.hwang@getac.com.tw>
2018-10-31 16:40:28 +0800

    theme

---
+++ PhwangGo/Phwang/Theme/Root/ThemeRootClass.cs
@@ -0,0 +1,46 @@
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Theme
+{
+    public class ThemeRootClass
+    {
+        private string objectName = "ThemeRootClass";
+
+        private UThemeClass uThemeObject { get; }
+        private DThemeClass dThemeObject { get; }
+
+       
+        public ThemeRootClass()
+        {
+            this.uThemeObject = new UThemeClass(this);
+            this.dThemeObject = new DThemeClass(this);
+        }
+
+        private void debugIt(bool on_off_val, string str0_val, string str1_val)
+        {
+            if (on_off_val)
+                this.logitIt(str0_val, str1_val);
+        }
+
+        private void logitIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangLogit(this.objectName + "." + str0_val + "()", str1_val);
+        }
+
+        private void abendIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangAbend(this.objectName + "." + str0_val + "()", str1_val);
+        }
+    }
+}
