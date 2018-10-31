54b0064e3d8d7389f512e324157363e45a23f101
paul.hwang <paul.hwang@getac.com.tw>
2018-10-31 16:55:11 +0800

    theme

---
+++ PhwangGo/Phwang/Engine/DEngine/DEngineClass.cs
@@ -0,0 +1,43 @@
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Engine
+{
+    public class DEngineClass
+    {
+        private string objectName = "DEngineClass";
+
+        private EngineRootClass engineRootObject { get; }
+
+        public DEngineClass(EngineRootClass engine_root_object_val)
+        {
+            this.engineRootObject = engine_root_object_val;
+        }
+
+        private void debugIt(bool on_off_val, string str0_val, string str1_val)
+        {
+            if (on_off_val)
+                this.logitIt(str0_val, str1_val);
+        }
+
+        private void logitIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangLogit(this.objectName + "." + str0_val + "()", str1_val);
+        }
+
+        private void abendIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangAbend(this.objectName + "." + str0_val + "()", str1_val);
+        }
+    }
+}
---
+++ PhwangGo/Phwang/Engine/Root/EngineRootClass.cs
@@ -0,0 +1,43 @@
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Engine
+{
+    public class EngineRootClass
+    {
+        private string objectName = "EngineRootClass";
+
+        private DEngineClass dEngineObject { get; }
+
+        public EngineRootClass()
+        {
+            this.dEngineObject = new DEngineClass(this);
+        }
+
+        private void debugIt(bool on_off_val, string str0_val, string str1_val)
+        {
+            if (on_off_val)
+                this.logitIt(str0_val, str1_val);
+        }
+
+        private void logitIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangLogit(this.objectName + "." + str0_val + "()", str1_val);
+        }
+
+        private void abendIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangAbend(this.objectName + "." + str0_val + "()", str1_val);
+        }
+    }
+}
