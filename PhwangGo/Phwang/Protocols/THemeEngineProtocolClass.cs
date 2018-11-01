765e12deb9fc2442db9de1e34672310345a2aa49
paul.hwang <paul.hwang@getac.com.tw>
2018-11-01 17:00:13 +0800

    .

---
+++ PhwangGo/Phwang/Protocols/THemeEngineProtocolClass.cs
@@ -0,0 +1,20 @@
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Protocols
+{
+    public class THemeEngineProtocolClass
+    {
+        public const int BASE_MGR_PROTOCOL_TRANSPORT_PORT_NUMBER = 8005;
+    }
+}
