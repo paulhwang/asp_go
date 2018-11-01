06f553b7dfde632ff1fe475b9d85bcf696dd6908
paul.hwang <paul.hwang@getac.com.tw>
2018-11-01 17:41:52 +0800

    .

--- PhwangGo/Phwang/Engine/DEngine/DEngineParserClass.cs
+++ PhwangGo/Phwang/Engine/DEngine/DEngineParserClass.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,12 +15,29 @@ namespace Phwang.Engine
 {
     public class DEngineParserClass
     {
+        private string objectName = "DEngineParserClass";
 
         private DEngineClass dEngineObject;
 
         public DEngineParserClass(DEngineClass d_engine_object_val)
         {
             this.dEngineObject = d_engine_object_val;
-    }
+        }
+
+        private void debugIt(bool on_off_val, string str0_val, string str1_val)
+        {
+            if (on_off_val)
+                this.logitIt(str0_val, str1_val);
+        }
+
+        private void logitIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangLogit(this.objectName + "." + str0_val + "()", str1_val);
+        }
+
+        private void abendIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangAbend(this.objectName + "." + str0_val + "()", str1_val);
+        }
     }
 }
