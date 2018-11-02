ef3ceafc5b5a0717b7eb3127f228755224cda549
paul.hwang <paul.hwang@getac.com.tw>
2018-11-02 11:17:07 +0800

    .

---
+++ PhwangGo/Phwang/Engine/Go/GoBaseClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Engine
+{
+    public class GoBaseClass
+    {
+        private string objectName = "GoBaseClass";
+
+        private PhwangUtils.ListEntryClass listEntryObject;
+        private string roomIdStr { get; }
+        private int baseId { get; set; }
+        private string baseIdStr { get; set; }
+
+        public string RoomIdStr() { return this.roomIdStr; }
+        public string BaseIdStr() { return this.baseIdStr; }
+
+        public GoBaseClass(string room_id_str_val)
+        {
+            this.roomIdStr = room_id_str_val;
+        }
+
+        public void BindListEntry(PhwangUtils.ListEntryClass list_entry_objectg_val)
+        {
+            this.listEntryObject = list_entry_objectg_val;
+            this.baseId = this.listEntryObject.Id;
+            this.baseIdStr = PhwangUtils.EncodeNumberClass.EncodeNumber(this.baseId, Protocols.ThemeEngineProtocolClass.ENGINE_BASE_ID_SIZE);
+        }
+    }
+}
--- PhwangGo/Phwang/Theme/Utheme/UThemeParserClass.cs
+++ PhwangGo/Phwang/Theme/Utheme/UThemeParserClass.cs
@@ -23,8 +23,93 @@ namespace Phwang.Theme
         {
             this.uThemeObject = u_theme_object_val;
         }
-        public void ParseInputPacket(string data_val)
+        public void ParseInputPacket(string input_data_val)
         {
+            this.debugIt(true, "ParseInputPacket", input_data_val);
+            string command = input_data_val.Substring(0, 1);
+            string input_data = input_data_val.Substring(1);
+
+            if (command == Protocols.ThemeEngineProtocolClass.THEME_ENGINE_PROTOCOL_RESPOND_IS_SETUP_BASE)
+            {
+                    this.processSetupBaseResponse(input_data);
+                    return;
+            }
+
+            if (command == Protocols.ThemeEngineProtocolClass.THEME_ENGINE_PROTOCOL_RESPOND_IS_PUT_BASE_DATA)
+            {
+                this.processPutBaseDataResponse(input_data);
+                return;
+            }
+
+            this.abendIt("exportedparseFunction", command);
+        }
+
+        private void processSetupBaseResponse(string input_data_val)
+        {
+            this.debugIt(true, "processSetupBaseResponse", input_data_val);
+
+            /*
+            char* room_id_index_val = data_val;
+
+            char* downlink_data;
+            char* data_ptr;
+            int group_array_size;
+
+            RoomClass* room = this->theThemeObject->searchRoom(room_id_index_val);
+            if (!room)
+            {
+                this->abend("processSetupBaseResponse", "null room");
+                return;
+            }
+
+            data_val += ROOM_MGR_PROTOCOL_ROOM_ID_INDEX_SIZE;
+            room->setBaseIdIndex(data_val);
+
+            downlink_data = data_ptr = (char*)phwangMalloc(ROOM_MGR_DATA_BUFFER_SIZE + 4, "UTSB");
+            *data_ptr++ = FABRIC_THEME_PROTOCOL_RESPOND_IS_SETUP_ROOM;
+
+            room->setGroupTableArray((char**)phwangArrayMgrGetArrayTable(room->groupArrayMgr(), &group_array_size));
+            memcpy(data_ptr, room->groupTableArray(0), GROUP_MGR_PROTOCOL_GROUP_ID_INDEX_SIZE);
+            data_ptr += GROUP_MGR_PROTOCOL_GROUP_ID_INDEX_SIZE;
+
+            memcpy(data_ptr, room->roomIdIndex(), ROOM_MGR_PROTOCOL_ROOM_ID_INDEX_SIZE);
+            data_ptr += ROOM_MGR_PROTOCOL_ROOM_ID_INDEX_SIZE;
+            *data_ptr = 0;
+            this->theThemeObject->dThemeObject()->transmitFunction(downlink_data);
+            */
+        }
+
+        private void processPutBaseDataResponse(string input_data_val)
+        {
+            this.debugIt(true, "processPutBaseDataResponse", input_data_val);
+            /*
+            char* downlink_data;
+            char* data_ptr;
+            int group_array_size;
+
+
+            RoomClass* room = this->theThemeObject->searchRoom(data_val);
+            if (!room)
+            {
+                this->abend("processPutBaseDataResponse", "null room");
+                return;
+            }
+            data_val += ROOM_MGR_PROTOCOL_ROOM_ID_INDEX_SIZE;
+
+            room->setGroupTableArray((char**)phwangArrayMgrGetArrayTable(room->groupArrayMgr(), &group_array_size));
+            for (int i = 0; i < group_array_size; i++)
+            {
+                if (room->groupTableArray(i))
+                {
+                    downlink_data = data_ptr = (char*)phwangMalloc(ROOM_MGR_DATA_BUFFER_SIZE + 4, "UTPB");
+                    *data_ptr++ = FABRIC_THEME_PROTOCOL_RESPOND_IS_PUT_ROOM_DATA;
+                    memcpy(data_ptr, room->groupTableArray(i), GROUP_MGR_PROTOCOL_GROUP_ID_INDEX_SIZE);
+                    data_ptr += GROUP_MGR_PROTOCOL_GROUP_ID_INDEX_SIZE;
+                    strcpy(data_ptr, data_val);
+                    this->theThemeObject->dThemeObject()->transmitFunction(downlink_data);
+                }
+            }
+            */
         }
 
         private void debugIt(bool on_off_val, string str0_val, string str1_val)
