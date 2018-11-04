401c183e47cb7230fdc47eae45253f0ff84bf09f
paul.hwang <phwang@gmail.com>
2018-11-04 22:08:31 +0800

    .

--- PhwangGo/Phwang/Engine/Go/GoGroupListClass.cs
+++ PhwangGo/Phwang/Engine/Go/GoGroupListClass.cs
@@ -13,27 +13,12 @@ using System.Threading.Tasks;
 
 namespace Phwang.Go
 {
-    public class GoFightClass
+    public class GoGroupListClass
     {
-        private string objectName = "GoFightClass";
-        private const int GO_FIGHT_CLASS_GROUP_LIST_ARRAY_SIZE = 7;
+        private string objectName = "GoGroupListClass";
 
-        private GoRootClass goRootObject { get; }
-        private bool abendEngineOn { get; }
-        GoGroupListClass[] groupListArray;
-        string theCaptureCount;
-        string theLastDeadStone;////////////////to be removed
-
-        public GoFightClass(GoRootClass go_root_object_val)
+        public GoGroupListClass()
         {
-            this.goRootObject = go_root_object_val;
-            this.groupListArray = new GoGroupListClass[GO_FIGHT_CLASS_GROUP_LIST_ARRAY_SIZE];
-
-        }
-
-        public void EnterBattle(GoMoveClass move_val)
-        {
-
         }
 
         private void debugIt(bool on_off_val, string str0_val, string str1_val)
