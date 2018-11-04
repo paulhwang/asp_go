d3868e63665a6cd8be54c1c810dae7a3f5edb92f
paul.hwang <phwang@gmail.com>
2018-11-04 08:47:57 +0800

    .

---
+++ PhwangGo/Phwang/Engine/Go/GoFightClass.cs
@@ -0,0 +1,43 @@
+﻿/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Go
+{
+    public class GoFightClass
+    {
+        private string objectName = "GoFightClass";
+
+        private GoRootClass goRootObject { get; }
+
+        public GoFightClass(GoRootClass go_root_object_val)
+        {
+            this.goRootObject = go_root_object_val;
+        }
+
+        private void debugIt(bool on_off_val, string str0_val, string str1_val)
+        {
+            if (on_off_val)
+                this.logitIt(str0_val, str1_val);
+        }
+
+        private void logitIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangLogit(this.objectName + "." + str0_val + "()", str1_val);
+        }
+
+        private void abendIt(string str0_val, string str1_val)
+        {
+            PhwangUtils.AbendClass.phwangAbend(this.objectName + "." + str0_val + "()", str1_val);
+        }
+    }
+}
