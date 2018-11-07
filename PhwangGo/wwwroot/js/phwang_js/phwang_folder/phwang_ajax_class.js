0afe2ced1a7d4a05918cf2cf259787105b5bdcbe
paul.hwang <phwang@gmail.com>
2018-11-07 22:37:56 +0800

    a

--- PhwangGo/wwwroot/js/phwang_js/phwang_folder/phwang_ajax_class.js
+++ PhwangGo/wwwroot/js/phwang_js/phwang_folder/phwang_ajax_class.js
@@ -108,9 +108,11 @@ function PhwangAjaxClass(phwang_object_val) {
 
                     if (data.charAt(0) === this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_RESPOND_IS_GET_LINK_DATA_PENDING_SESSION()) {
                         this.debug(true, "getLinkDataResponse", "pending_session_data=" + data);
-                        var session_id = data.slice(1, this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_SESSION_ID_SIZE() + 1);
+                        var link_id = data.slice(1, 1 + this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_LINK_ID_SIZE());
+                        this.debug(true, "getLinkDataResponse", "link_id=" + link_id);
+                        var session_id = data.slice(1 + this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_LINK_ID_SIZE(), 1 + this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_LINK_SESSION_ID_SIZE());
                         this.debug(true, "getLinkDataResponse", "session_id=" + session_id);
-                        data = data.slice(1 + this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_SESSION_ID_SIZE());
+                        data = data.slice(1 + this.phwangAjaxProtocolObject().WEB_FABRIC_PROTOCOL_LINK_SESSION_ID_SIZE());
                         var theme_data = data;
                         this.debug(true, "getLinkDataResponse", "theme_data=" + theme_data);
                         var config_len = this.phwangObject().decodeNumber(theme_data.slice(1), 3);
