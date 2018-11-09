1308315079409fa6cac95b5b69b8a5eca75b86d4
phwang <phwang@gmail.com>
2018-11-09 16:54:30 +0800

    .

---
+++ PhwangGo/wwwroot/js/phwang_js/go_folder/go_config_dir/go_config_html.js
@@ -0,0 +1,53 @@
+﻿/*
+  Copyrights reserved
+  Written by Paul Hwang
+*/
+
+class Hello extends React.Component {
+    render() {
+        return React.createElement(
+            "body",
+            null,
+            React.createElement(
+                "h2",
+                null,
+                "Account Sign In"
+            ),
+            React.createElement(
+                "section",
+                { "className": "login_section" },
+                React.createElement(
+                    "p",
+                    null,
+                    "Name:",
+                    React.createElement("input", {
+                        type: "text",
+                        "className": "login_name",
+                        placeholder: "Enter your name"
+                    })
+                ),
+                React.createElement(
+                    "p",
+                    null,
+                    "Password:",
+                    React.createElement("input", {
+                        type: "text",
+                        "className": "login_password",
+                        placeholder: "Enter your password"
+                    })
+                ),
+                React.createElement(
+                    "button",
+                    { "className": "login_button" },
+                    "Login"
+                )
+            )
+        );
+    }
+}
+
+ReactDOM.render(
+    React.createElement(Hello, null, null),
+    document.getElementById('go_config_html')
+);
+
