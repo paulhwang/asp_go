314df910228471d16823ee4162a12bd973398ecb
paul.hwang <paul.hwang@getac.com.tw>
2018-11-09 15:24:55 +0800

    .

--- PhwangGo/wwwroot/js/phwang_js/account_folder/account_sign_in_html.js
+++ PhwangGo/wwwroot/js/phwang_js/account_folder/account_sign_in_html.js
@@ -1,25 +1,49 @@
 ﻿
-
-ReactDOM.render(
-    React.DOM.h2(null, "hi phwang!!!"),
-    document.getElementById("account_sign_in_html")
-);
-
 class Hello extends React.Component {
     render() {
-        return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+        return React.createElement(
+            "body",
+            null,
+            React.createElement(
+                "h2",
+                null,
+                "Account Sign In"
+            ),
+            React.createElement(
+                "section",
+                { "class": "login_section" },
+                React.createElement(
+                    "p",
+                    null,
+                    "Name:",
+                    React.createElement("input", {
+                        type: "text",
+                        "class": "login_name",
+                        placeholder: "Enter your name"
+                    })
+                ),
+                React.createElement(
+                    "p",
+                    null,
+                    "Password:",
+                    React.createElement("input", {
+                        type: "text",
+                        "class": "login_password",
+                        placeholder: "Enter your password"
+                    })
+                ),
+                React.createElement(
+                    "button",
+                    { "class": "login_button" },
+                    "Login"
+                )
+            )
+        );
     }
 }
 
 ReactDOM.render(
-    React.createElement(Hello, { toWhat: 'World' }, null),
-    document.getElementById('root')
+    React.createElement(Hello, null, null),
+    document.getElementById('account_sign_in_html')
 );
 
-
-const e = React.createElement;
-
-ReactDOM.render(
-    e('div', null, 'Hello phwang!!!'),
-    document.getElementById('root0')
-);
