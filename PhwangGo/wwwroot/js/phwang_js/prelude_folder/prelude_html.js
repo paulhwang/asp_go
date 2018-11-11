bd15e4d7fb03acd3ca2ef5b3a163df54279941d5
paul.hwang <phwang@gmail.com>
2018-11-11 13:39:26 +0800

    prelude

---
+++ PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_html.js
@@ -0,0 +1,23 @@
+﻿/*
+  Copyrights reserved
+  Written by Paul Hwang
+*/
+
+class PhwangPreludeComponentClass extends React.Component {
+    render() {
+        return React.createElement("body", null,
+            React.createElement("h1", null, "Let's Play Go!"),
+            React.createElement("p", { "className": "lead" }, "Let's Play Go is a free web platform to play Go Game with people in the world."),
+
+            //<p><a href="http://localhost:8168/Account/AccountSignUp" class="btn btn-primary btn-lg">Sign up &raquo;</a></p>
+            //<p><a href="http://localhost:8168/Account/AccountSignIn" class="btn btn-primary btn-lg">Sign in &raquo;</a></p>
+            //<p><a href="http://localhost:8168/go/goroot" class="btn btn-primary btn-lg">Play Go &raquo;</a></p>
+
+            React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/Account/AccountSignUp", "className": "btn btn-primary btn-lg" }, "Sign up")),
+            React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/Account/AccountSignIn", "className": "btn btn-primary btn-lg" }, "Sign in")),
+            React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/go/goroot", "className": "btn btn-primary btn-lg" }, "Play Go"))
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(PhwangPreludeComponentClass, null, null), document.getElementById("phwang_prelude"));
