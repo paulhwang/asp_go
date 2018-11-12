1be8b8f3938e2e10e512849f230f6fac5b269a91
paul.hwang <paul.hwang@getac.com.tw>
2018-11-12 09:29:52 +0800

    a

--- PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_html.js
+++ PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_html.js
@@ -10,7 +10,6 @@ class PhwangPreludeComponentClass extends React.Component {
             React.createElement("p", { "className": "lead" }, "Let's Play Go is a free web platform to play Go Game with people in the world."),
             React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/Account/AccountSignUp", "className": "btn btn-primary btn-lg" }, "Sign up")),
             React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/Account/AccountSignIn", "className": "btn btn-primary btn-lg" }, "Sign in")),
-            React.createElement("p", null, React.createElement("a", { "href": "http://localhost:8168/go/goroot", "className": "btn btn-primary btn-lg" }, "Play Go"))
             //<p><a href="http://localhost:8168/Account/AccountSignUp" class="btn btn-primary btn-lg">Sign up &raquo;</a></p>
         );
     }
