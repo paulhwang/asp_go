6d08e0960a14d4d36b673c0b2839c85d98416088
paul.hwang <phwang@gmail.com>
2018-11-16 23:33:18 +0800

    a

--- PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_render.js
+++ PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_render.js
@@ -3,10 +3,164 @@
   Written by Paul Hwang
 */
 
-function PreludeRenderObject(){"use strict";
-    this.init__ = function (root_object_val){
+function PreludeRenderObject(root_object_val) {
+    "use strict";
+    this.init__ = function (){
         this.theRootObject = root_object_val;
     }
+    this.theDispalySwitch = {
+        prelude_on: false,
+        sign_up_on: false,
+        sign_in_on: false,
+        theme_on: false,
+        go_config_on: false,
+        go_game_on: false
+    }
+    this.dispalySwitch = function () { return this.theDispalySwitch; }
+    this.setupPreludeSwitch = function () {
+        this.theDispalySwitch.prelude_on = true;
+        this.theDispalySwitch.sign_up_on = false;
+        this.theDispalySwitch.sign_in_on = false;
+        this.theDispalySwitch.theme_on = false;
+        this.theDispalySwitch.go_config_on = false;
+        this.theDispalySwitch.go_game_on = false;
+    }
+    this.renderPreludePage = function () {
+        this.setupPreludeSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        var this0 = this;
+        $(".prelude_section .sign_up_button").on("click", function () {
+            this0.debug(true, "renderPreludePage click function", ".prelude_section .sign_up_button");
+            this0.renderSignUpPage();
+        });
+        $(".prelude_section .sign_in_button").on("click", function () {
+            this0.debug(true, "renderPreludePage click function", ".prelude_section .sign_in_button");
+            this0.renderSignInPage();
+        });
+        $(".prelude_section .theme_button").on("click", function () {
+            this0.debug(true, "renderPreludePage click function", ".prelude_section .theme_button");
+            this0.renderThemePage();
+        });
+    };
+    this.setupSignUpSwitch = function () {
+        this.theDispalySwitch.prelude_on = false;
+        this.theDispalySwitch.sign_up_on = true;
+        this.theDispalySwitch.sign_in_on = false;
+        this.theDispalySwitch.theme_on = false;
+        this.theDispalySwitch.go_config_on = false;
+        this.theDispalySwitch.go_game_on = false;
+    }
+    this.renderSignUpPage = function () {
+        //ReactDOM.render(React.createElement(SignUpComponentClass, null, null), document.getElementById("phwang_prelude"));
+        this.setupSignUpSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        var this0 = this;
+        $(".sign_up_section .sign_up_button").on("click", function () {
+            this0.debug(true, "renderSignUpPage click function", ".sign_up_section .sign_up_button");
+            this0.renderPreludePage();
+        });
+    };
+    this.setupSignInSwitch = function () {
+        this.theDispalySwitch.prelude_on = false;
+        this.theDispalySwitch.sign_up_on = false;
+        this.theDispalySwitch.sign_in_on = true;
+        this.theDispalySwitch.theme_on = false;
+        this.theDispalySwitch.go_config_on = false;
+        this.theDispalySwitch.go_game_on = false;
+    }
+    this.renderSignInPage = function () {
+        //ReactDOM.render(React.createElement(SignInComponentClass, null, null), document.getElementById("phwang_prelude"));
+        this.setupSignInSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        var this0 = this;
+        $(".sign_in_section .sign_in_button").on("click", function () {
+            this0.debug(true, "renderSignUpPage click function", ".sign_in_section .sign_in_button");
+            this0.phwangLinkObject().setMyName($(".sign_in_section .sign_in_name").val());
+            var password = $(".sign_in_section .sign_in_password").val();
+            this0.debug(true, "renderSignInPage", "myName=" + this0.phwangLinkObject().myName() + " password=" + password);
+            if (this0.phwangLinkObject().myName()) {
+                this0.phwangAjaxObject().setupLink(this0.phwangLinkObject(), password);
+            }
+            //this0.renderThemePage();
+        });
+    };
+    this.setupThemeSwitch = function () {
+        this.theDispalySwitch.prelude_on = false;
+        this.theDispalySwitch.sign_up_on = false;
+        this.theDispalySwitch.sign_in_on = false;
+        this.theDispalySwitch.theme_on = true;
+        this.theDispalySwitch.go_config_on = false;
+        this.theDispalySwitch.go_game_on = false;
+    }
+    this.renderThemePage = function () {
+        this.phwangAjaxObject().startWatchDog(this.phwangLinkObject());
+        //ReactDOM.render(React.createElement(ThemeComponentClass, null, null), document.getElementById("phwang_prelude"));
+        this.setupThemeSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        var this0 = this;
+        $(".theme_section .go_button").on("click", function () {
+            this0.debug(true, "renderThemePage click function", ".theme_section .go_button");
+            this0.renderGoConfigPage();
+        });
+    };
+    this.setupGoConfigSwitch = function () {
+        this.theDispalySwitch.prelude_on = false;
+        this.theDispalySwitch.sign_up_on = false;
+        this.theDispalySwitch.sign_in_on = false;
+        this.theDispalySwitch.theme_on = false;
+        this.theDispalySwitch.go_config_on = true;
+        this.theDispalySwitch.go_game_on = false;
+    }
+    this.renderGoConfigPage = function () {
+        this.rootObject().startNewGoGame();
+        this.setupGoConfigSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        this.renderNameList();
+        var this0 = this;
+        $(".config_section .config_button").on("click", function () {
+            this0.configStorageObject().setHisName($(".peer_name_paragraph select").val());
+            this0.configStorageObject().setMyColor($(".config_section .go_config_section .stone_color").val());
+            this0.configStorageObject().setBoardSize($(".config_section .go_config_section .board_size").val());
+            this0.configStorageObject().setKomiPoint($(".config_section .go_config_section .komi").val());
+            this0.configStorageObject().setHandicapPoint($(".config_section .go_config_section .handicap").val());
+            var encoded_config = this0.configStorageObject().encodeConfig(this0.phwangLinkObject().myName());
+            this0.debug(true, "setupHtmlInput", "boardSize=" + this0.configStorageObject().boardSize() + " myColor=" + this0.configStorageObject().myColor() + " komi=" + this0.configStorageObject().komiPoint() + " handicap=" + this0.configStorageObject().handicapPoint());
+            this0.phwangAjaxObject().setupSession(this0.phwangLinkObject(), this0.configStorageObject().hisName(), encoded_config);
+            this0.renderGoGamePage();
+        });
+        //window.open(this.phwangObject().serverHttpHeader() + "Go/GoSetup", "_self");
+    };
+    this.renderNameList = function () {
+        for (var i = 0; i < this.phwangLinkObject().nameListLength(); i++) {
+            $('.peer_name_paragraph select').append($('<option>', { value: this.phwangLinkObject().nameListElement(i), text: this.phwangLinkObject().nameListElement(i) }));
+        }
+    };
+    this.setupGoGameSwitch = function () {
+        this.theDispalySwitch.prelude_on = false;
+        this.theDispalySwitch.sign_up_on = false;
+        this.theDispalySwitch.sign_in_on = false;
+        this.theDispalySwitch.theme_on = false;
+        this.theDispalySwitch.go_config_on = false;
+        this.theDispalySwitch.go_game_on = true;
+    }
+    this.renderGoGamePage = function () {
+        this.setupGoGameSwitch();
+        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, this.dispalySwitch()), document.getElementById("phwang_prelude"));
+        this.goBaseObject().init_game();
+    };
+    this.objectName = function () { return "PreludeRenderObject"; };
+    this.rootObject = function () { return this.theRootObject; };
+    this.phwangObject = function () { return this.rootObject().phwangObject(); };
+    this.phwangAjaxObject = function () { return this.phwangObject().phwangAjaxObject(); };
+    this.phwangLinkObject = function () { return this.phwangObject().phwangLinkObject(); };
+    this.goBaseObject = function () { return this.rootObject().goBaseObject(); };
+    this.configStorageObject = function () { return this.goBaseObject().configStorageObject(); };
+    this.configObject = function () { return this.goBaseObject().configObject(); };
+    this.debug = function (debug_val, str1_val, str2_val) { if (debug_val) { this.logit(str1_val, str2_val); } };
+    this.logit = function (str1_val, str2_val) { return this.logit_(this.objectName() + "." + str1_val, str2_val); };
+    this.abend = function (str1_val, str2_val) { return this.abend_(this.objectName() + "." + str1_val, str2_val); };
+    this.logit_ = function (str1_val, str2_val) { this.phwangObject().LOG_IT(str1_val, str2_val); };
+    this.abend_ = function (str1_val, str2_val) { this.phwangObject().ABEND(str1_val, str2_val); };
     this.init__();
 }
 
