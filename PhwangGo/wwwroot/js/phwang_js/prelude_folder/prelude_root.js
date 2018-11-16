279c745cb48d37bf583790d3faee2b2710863ef8
paul.hwang <phwang@gmail.com>
2018-11-16 23:38:36 +0800

    a

--- PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_root.js
+++ PhwangGo/wwwroot/js/phwang_js/prelude_folder/prelude_root.js
@@ -2,14 +2,14 @@
   Copyrights reserved
   Written by Paul Hwang
 */
-function PreludeRootObject(){"use strict";
-    this.init__ = function(){
+function PreludeRootObject() {
+    "use strict";
+    this.init__ = function() {
         this.thePhwangObject = new PhwangClass(this);
         this.thePreludeRenderObject = new PreludeRenderObject(this);
         this.phwangObject().initObject();
         this.theAjaxObject = new PreludeAjaxClass(this);
         this.preludeRenderObject().renderPreludePage();
-        this.debug(true, "init__", "myName=" + this.phwangLinkObject().myName() + " linkId=" + this.phwangLinkObject().linkId());
     };
     this.objectName = function(){return "PreludeRootObject";};
     this.phwangObject = function () { return this.thePhwangObject; };
@@ -35,40 +35,8 @@ function PreludeRootObject(){"use strict";
     ////////////////////////////////////////////////////////////////////////////////////////////////
     this.init__();
 }
-function LoginHtmlObject(root_object_val){"use strict";
-    this.componentName = function(){return "account_sign_in_html";};
-    this.gotoNextPage = function(){ window.open(this.phwangObject().serverHttpHeader() + "Go/GoSetup", "_self") };
-    this.init__ = function(root_object_val){ this.theRootObject = root_object_val; this.setupHtmlInput();};
-    this.setupHtmlInput = function(){
-        ReactDOM.render(React.createElement(PhwangPreludeComponentClass, null, null), document.getElementById("phwang_prelude"));
-        var this0 = this;
-        $(".prelude_section .sign_up_button").on("click", function(){
-            this0.debug(true, "click function", ".prelude_section .sign_up_button");
-            ReactDOM.render(React.createElement(SignUpComponentClass, null, null), document.getElementById("phwang_prelude"));
-            $(".sign_up_section .sign_up_button").on("click", function(){
-                this0.debug(true, "click function", ".sign_up_section .sign_up_button");
-                ReactDOM.render(React.createElement(PhwangPreludeComponentClass, null, null), document.getElementById("phwang_prelude"));
-
-            });
-        });
-        $(".sign_up_section .sign_up_button").on("click", function(){
-            this0.debug(true, "click function", ".sign_up_section .sign_up_button");
-            ReactDOM.render(React.createElement(PhwangPreludeComponentClass, null, null), document.getElementById("phwang_prelude"));
-
-        });
-    };
-    this.objectName = function(){return "LoginHtmlObject";};
-    this.rootObject = function(){return this.theRootObject;};
-    this.phwangObject = function(){return this.rootObject().phwangObject();};
-    this.phwangAjaxObject = function(){return this.phwangObject().phwangAjaxObject();};
-    this.phwangLinkObject = function(){return this.phwangObject().phwangLinkObject();};
-    this.ajaxObject = function(){return this.rootObject().ajaxObject();};
-    this.debug = function(debug_val, str1_val, str2_val){ if (debug_val){ this.logit(str1_val, str2_val);} };
-    this.logit = function(str1_val, str2_val){return this.rootObject().logit_(this.objectName() + "." + str1_val, str2_val);};
-    this.abend = function(str1_val, str2_val){return this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
-    this.init__(root_object_val);
-}
-function PreludeAjaxClass(root_object_val){"use strict";
+function PreludeAjaxClass(root_object_val) {
+    "use strict";
     this.init__ = function(root_object_val){ this.theRootObject = root_object_val;};
     this.receiveSetupLinkResponse = function (result_val) { this.preludeRenderObject().renderThemePage();};
     this.receiveGetNameListResponse = function(result_val){};
@@ -87,6 +55,6 @@ function PreludeAjaxClass(root_object_val){"use strict";
     this.abend = function(str1_val, str2_val){return this.rootObject().abend_(this.objectName() + "." + str1_val, str2_val);};
     this.init__(root_object_val);
 }
-var login_main = function(){"use strict"; new PreludeRootObject();};
+var login_main = function() {"use strict"; new PreludeRootObject();};
 //$(document).ready(login_main);
 window.onload = login_main;
