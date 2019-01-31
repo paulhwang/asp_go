585f1947d020931bb4d3d8ee637162eda8af4640
phwang <phwang@gmail.com>
2019-01-31 16:12:53 +0800

    add

---
+++ PhwangGo/Phwang/Models/AccountOperationClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phwang.Models
+{
+    public class AccountOperationClass
+    {
+        public void Create(AccountClass item_val)
+        {
+
+        }
+
+        public void Update(AccountClass item_val)
+        {
+
+        }
+
+        public void Delete(AccountClass item_val)
+        {
+
+        }
+    }
+}
